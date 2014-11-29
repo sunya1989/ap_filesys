49c0fda157c593f2da5dad600d94e781cf553f16
sunya1989 <goingonhxk@gmail.com>
2014-11-29 13:51:57 +0800

    ger_fs add

---
+++ ap_file_system/ger/ger_fs.h
@@ -0,0 +1,21 @@
+//
+//  ger_fs.h
+//  ap_file_system
+//
+//  Created by sunya on 14/11/29.
+//  Copyright (c) 2014年 sunya. All rights reserved.
+//
+
+#ifndef ap_file_system_ger_fs_h
+#define ap_file_system_ger_fs_h
+enum file_state{
+    fileno = 0,
+    stream,
+};
+
+
+struct ger_struct{
+    
+};
+
+#endif
